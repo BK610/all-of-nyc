ff49f11e0e59698f2a50bb20a7a5ea72ee8d41a7
Bailey Kane <bailey.orion.kane@gmail.com>
2024-11-15 22:13:41 -0500

    Added dark mode support and custom color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'media', // Use the 'media' strategy to follow system preferences
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'nyc-blue': '#0067AC',
+        'soft-blue': '#66B2FF',
+        'pastel-blue': '#93C5FD',
+        'nyc-orange': '#FF8200',
+        'nyc-dark-gray': '#333333',
+        'nyc-light-gray': '#A7A8AC',
+        'dark-bg': '#121212',
+        'dark-card': '#1E1E1E',
       },
     },
   },
