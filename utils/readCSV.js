20c1a5a6e70c1568e0ce84e07fb88cd378dbbc5d
Bailey Kane <bailey.orion.kane@gmail.com>
2024-11-15 20:55:22 -0500

    edits

--- utils/readCSV.js
+++ utils/readCSV.js
@@ -1,11 +1,20 @@
+import fs from 'fs';
 import Papa from 'papaparse';
 import path from 'path';
 
-export const getUrlsFromCSV = () => {
-  const fs = require('fs');
+export function getUrlsFromCSV(page = 1, pageSize = 20) {
   const filePath = path.join(process.cwd(), '/nyc_Domain_Registrations_20241115.csv');
   const fileContent = fs.readFileSync(filePath, 'utf8');
   
+  // Parse CSV data
   const { data } = Papa.parse(fileContent, { header: true });
-  return data;
+
+  // Paginate the data
+  const startIndex = (page - 1) * pageSize;
+  const paginatedData = data.slice(startIndex, startIndex + pageSize);
+
+  return {
+    urls: paginatedData,
+    total: data.length,
+  };
 };
