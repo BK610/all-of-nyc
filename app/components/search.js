6f024b59b5b31b5ee9f600f3150daa4a65f76143
BK610 <bailey.orion.kane@gmail.com>
2024-11-20 17:30:03 -0500

    Update styling to show a reasonably-sized search box on mobile

--- app/components/search.js
+++ app/components/search.js
@@ -10,13 +10,13 @@ const Search = ({onSearch}) => {
     };
 
     return (
-      <form onSubmit={handleSearch} className="mb-8 flex justify-center items-center gap-4">
+      <form onSubmit={handleSearch} className="mb-2 flex justify-center items-center gap-4">
         <input
           type="text"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
           placeholder="Search URLs..."
-          className="w-1/3 px-4 py-2 border border-nyc-light-gray rounded-lg text-gray-900 placeholder-gray-500 focus:outline-none focus:ring focus:ring-nyc-blue"
+          className="w-8/12 md:w-1/3 px-4 py-2 border border-nyc-light-gray rounded-lg text-gray-900 placeholder-gray-500 focus:outline-none focus:ring focus:ring-nyc-blue"
         />
         <button
           type="submit"
