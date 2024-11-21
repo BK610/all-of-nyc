65f5f2070fc6f194a28b6892e407ecc3454e20e7
BK610 <bailey.orion.kane@gmail.com>
2024-11-20 19:03:16 -0500

    Minor styling updates

--- app/components/notebookEmbed.js
+++ app/components/notebookEmbed.js
@@ -13,10 +13,10 @@ const NotebookEmbed = ({src, fallbackUrl}) => {
 
   return (
     <div className="w-full py-10">
-      
-        This is a rendered Jupyter notebook.
-        If it isn't rendering correctly, visit <a href={fallbackUrl} target='_blank' className='underline' >{fallbackUrl}</a>.
-      
+      <p>
+        This is a rendered <a href='https://jupyter.org/' target='_blank' className='text-nyc-blue hover:text-nyc-blue hover:underline'>Jupyter</a> notebook.
+        If it isn't rendering correctly, visit <a href={fallbackUrl} target='_blank' className='text-nyc-blue hover:text-nyc-blue hover:underline' >{fallbackUrl}</a>.
+      </p>
       <div className="w-full" dangerouslySetInnerHTML={{ __html: html }}/>
     </div>
   );
