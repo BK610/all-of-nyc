45a4d83625a99eb74d2fe711eb763f2f0877cffb
BK610 <bailey.orion.kane@gmail.com>
2024-11-22 21:46:06 -0500

    Remove transition-colors CSS

--- app/components/search.js
+++ app/components/search.js
@@ -20,7 +20,7 @@ const Search = ({onSearch}) => {
         />
         <button
           type="submit"
-          className="px-4 py-2 bg-nyc-orange text-white rounded-lg hover:bg-orange-700 transition-colors"
+          className="px-4 py-2 bg-nyc-orange text-white rounded-lg hover:bg-orange-700"
         >
           Search
         </button>
