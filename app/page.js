31e0ab1dac80a13be9e22a8db375eaea737cba54
BK610 <bailey.orion.kane@gmail.com>
2024-12-06 15:51:55 -0500

    Minor formatting changse

--- app/page.js
+++ app/page.js
@@ -60,11 +60,7 @@ export default function Home() {
         />
         {/* URL Cards */}
         {urls.length > 0 ? ( // grid-cols-1 md:grid-cols-2 lg:grid-cols-3
-          <div
-            className="columns-1 md:columns-2 lg:columns-3
-          
-           gap-5"
-          >
+          <div className="columns-1 md:columns-2 lg:columns-3 gap-5">
             {urls.map((url, index) => (
               <Card key={index} url={url} />
             ))}
