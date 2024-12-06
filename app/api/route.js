4d82e7d0d954286a987c591b053f4360b3bbef97
BK610 <bailey.orion.kane@gmail.com>
2024-12-06 14:07:32 -0500

    Swapped raw URL for env variable

--- app/api/route.js
+++ app/api/route.js
@@ -1,7 +1,7 @@
-import { NextResponse } from 'next/server';
-import Papa from 'papaparse';
+import { NextResponse } from "next/server";
+import Papa from "papaparse";
 
-const GOOGLE_SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTmO4zZCoQVROb9q_pRRLZG4voK03u3eIzAcsy69R9Q2bAqejFO41_1SzWSoA1m83p8HiNu3xJyiZ1J/pub?gid=1960351903&single=true&output=csv';
+const GOOGLE_SHEET_URL = process.env.GOOGLE_SHEET_URL;
 
 let cachedData = null;
 let lastFetchedTime = 0;
@@ -11,45 +11,49 @@ const CACHE_DURATION_MS = 5 * 60 * 1000;
 
 export async function GET(request) {
   // Pagination and Search info from the request parameters
-  const page = parseInt(request.nextUrl.searchParams.get('page') || '1', 10);
-  const pageSize = parseInt(request.nextUrl.searchParams.get('pageSize') || '15', 10);
-  const searchQuery = request.nextUrl.searchParams.get('search')?.toLowerCase() || '';
-  
+  const page = parseInt(request.nextUrl.searchParams.get("page") || "1", 10);
+  const pageSize = parseInt(
+    request.nextUrl.searchParams.get("pageSize") || "15",
+    10
+  );
+  const searchQuery =
+    request.nextUrl.searchParams.get("search")?.toLowerCase() || "";
+
   const currentTime = Date.now();
-  
+
   // Check if cached data is still valid
   if (cachedData && currentTime - lastFetchedTime < CACHE_DURATION_MS) {
-    console.log('Using cached data');
+    console.log("Using cached data");
 
     // Filter data based on search query if provided
     const filteredData = searchQuery
-    ? cachedData.filter((row) => row.url?.toLowerCase().includes(searchQuery))
-    : cachedData;
-
-    return paginateData(filteredData, page, pageSize);
-  }
-  
-    console.log("Fetching new data...");
-    try {
-      const data = await fetchCSVData();
-      cachedData = data;
-      lastFetchedTime = currentTime;
-
-      // Filter data based on search query if provided
-      const filteredData = searchQuery
       ? cachedData.filter((row) => row.url?.toLowerCase().includes(searchQuery))
       : cachedData;
 
-      return paginateData(filteredData, page, pageSize);
-    } catch (error) {
-      console.error('Error fetching Google Sheet CSV:', error);
-      return NextResponse.json({ error: error.message }, { status: 500 });
+    return paginateData(filteredData, page, pageSize);
+  }
+
+  console.log("Fetching new data...");
+  try {
+    const data = await fetchCSVData();
+    cachedData = data;
+    lastFetchedTime = currentTime;
+
+    // Filter data based on search query if provided
+    const filteredData = searchQuery
+      ? cachedData.filter((row) => row.url?.toLowerCase().includes(searchQuery))
+      : cachedData;
+
+    return paginateData(filteredData, page, pageSize);
+  } catch (error) {
+    console.error("Error fetching Google Sheet CSV:", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
 
 async function fetchCSVData() {
   const response = await fetch(GOOGLE_SHEET_CSV_URL);
-  if (!response.ok) throw new Error('Failed to fetch data');
+  if (!response.ok) throw new Error("Failed to fetch data");
 
   // Read the response as text (CSV content)
   const csvText = await response.text();
