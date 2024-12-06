59ce8e5c2e79b1d6a6d5bf00bb9522cd8d8f927b
BK610 <bailey.orion.kane@gmail.com>
2024-12-06 13:53:07 -0500

    Add disabled styling if we didn't find the URL successfully

--- app/components/card.js
+++ app/components/card.js
@@ -1,15 +1,29 @@
 const Card = ({ url }) => {
   if (!url) return null; // Handle null or undefined objects gracefully
 
+  let found_url = true;
+
+  // Check if we successfully got info about the URL
+  if (
+    !url.final_url ||
+    url.final_url == "Error" ||
+    url.final_url == "Not found"
+  ) {
+    found_url = false;
+  }
+
   return (
     <a
-      href={url.final_url ? url.final_url : ""}
-      target="_blank"
+      href={found_url ? url.final_url : ""} // Set href to "" if found_url is false
+      target={found_url ? "_blank" : ""} // Set target to "" if found_url is false
       rel="noopener noreferrer"
       className={`block bg-white shadow-lg rounded-lg p-6 border border-gray-200 transition-transform transform hover:scale-105 hover:shadow-xl
-            `}
+        ${found_url ? "" : "pointer-events-none bg-gray-100 opacity-70"}`} // Set disabled-esque styling if found_url is false
     >
-      <h2 className="text-nyc-blue text-xl font-semibold mb-2">{url.url}</h2>
+      <h2 className="text-nyc-blue text-xl font-semibold mb-2">
+        {url.url}&nbsp;
+        {found_url ? "✅" : "💀"}
+      </h2>
       <p className="text-gray-600 mt-2">Registered: {url.registration_date}</p>
     </a>
   );
