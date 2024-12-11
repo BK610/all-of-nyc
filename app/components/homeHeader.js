4b2f4b452578605ce1821ec82f03ef02a640c49e
BK610 <bailey.orion.kane@gmail.com>
2024-12-11 09:37:05 -0500

    Move homepage header into a component to make homepage information structure clearer

---
+++ app/components/homeHeader.js
@@ -0,0 +1,14 @@
+const HomeHeader = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-nyc-blue text-center mb-2">
+        All of .nyc
+      </h1>
+      <p className="text-center mb-4">
+        Discover how .nyc domains are being used.
+      </p>
+    </>
+  );
+};
+
+export default HomeHeader;
