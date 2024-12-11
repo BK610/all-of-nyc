16aeff82bb38800ea9b5e3b57331411b8fb33d63
BK610 <bailey.orion.kane@gmail.com>
2024-12-11 09:29:27 -0500

    Renamed to DomainCard. Update variables to (1) use camelCase if they didn't already, and (2) have more descriptive, accurate names

--- app/components/domainCard.js
+++ app/components/domainCard.js
@@ -2,11 +2,11 @@
  *
  * The name "Card" sounds like a generic. "DomainCard" or "UrlCard" might be more descriptive.
  */
-const Card = ({ url }) => {
+const DomainCard = ({ url }) => {
   if (!url) return null; // Handle null or undefined objects gracefully
 
   // Check if we successfully got info about the URL
-  let found_url = !(
+  let isUrlFound = !(
     !url.final_url ||
     url.final_url == "Error" ||
     url.final_url == "Not found"
@@ -34,14 +34,14 @@ const Card = ({ url }) => {
   //   url.status_code.substring(0, 1) == 3;
 
   // Check if we successfully got an Open Graph title
-  let found_title = !(
+  let isTitleFound = !(
     !url.title ||
     url.title == "Error" ||
     url.title == "Not found"
   );
 
   // Check if we successfully got an Open Graph image
-  let found_image = !(
+  let isImageFound = !(
     !url.image ||
     url.image == "Error" ||
     url.image == "Not found"
@@ -62,13 +62,13 @@ const Card = ({ url }) => {
 
   return (
     <a
-      href={found_url ? url.final_url : ""} // Set href to "" if found_url is false
-      target={found_url ? "_blank" : ""} // Set target to "" if found_url is false
+      href={isUrlFound ? url.final_url : ""} // Set href to "" if found_url is false
+      target={isUrlFound ? "_blank" : ""} // Set target to "" if found_url is false
       rel="noopener noreferrer"
       className={`inline-block w-full mb-4 overflow-hidden bg-white shadow-lg rounded-lg border border-gray-200 transition-transform transform hover:scale-105 hover:shadow-xl
-        ${found_url ? "" : "pointer-events-none bg-gray-100 opacity-70"}`} // Set disabled-esque styling if found_url is false
+        ${isUrlFound ? "" : "pointer-events-none bg-gray-100 opacity-70"}`} // Set disabled-esque styling if found_url is false
     >
-      {found_image ? (
+      {isImageFound ? (
         <div className="w-full">
           <img
             className="object-cover w-full h-52 border-b-2"
@@ -81,13 +81,13 @@ const Card = ({ url }) => {
       <div className="p-5">
         <h2 className="text-nyc-blue text-xl font-semibold mb-2">
           {url.url}&nbsp;
-          {found_url ? <>{found_title ? "✅" : "❓"}</> : "💀"}
+          {isUrlFound ? <>{isTitleFound ? "✅" : "❓"}</> : "💀"}
         </h2>
         <p className="mb-1">
-          <b>Title:</b> {found_url ? url.title : "N/A"}
+          <b>Title:</b> {isUrlFound ? url.title : "N/A"}
         </p>
         <p className="mb-1 line-clamp-3">
-          <b>Description:</b> {found_url ? url.description : "N/A"}
+          <b>Description:</b> {isUrlFound ? url.description : "N/A"}
         </p>
         <p className="text-gray-600 pt-2 w-full border-t-2">
           Final URL: {url.final_url}
@@ -99,4 +99,4 @@ const Card = ({ url }) => {
   );
 };
 
-export default Card;
+export default DomainCard;
