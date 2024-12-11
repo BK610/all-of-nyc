338265d591db5c6de509683f2a5181a9b9e60e27
BK610 <bailey.orion.kane@gmail.com>
2024-12-11 18:50:38 -0500

    Add asynchronous processing mode, to dramatically speed up processing of rows. Upgrade CLI arg handling to enable optional async mode

--- utils/enrich_urls.py
+++ utils/enrich_urls.py
@@ -1,9 +1,11 @@
 import requests
 import pandas as pd
-from populate_metadata import ensure_protocol, get_status_code, get_meta_data
+from populate_metadata import ensure_protocol, get_status_code, get_meta_data, get_status_code_async, get_meta_data_async
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import os
-import sys
+import aiohttp
+import asyncio
+import argparse
 
 CSV_ROWS_SCHEMA = [
     'url',
@@ -27,6 +29,15 @@ def setup_session():
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
     return session
+
+def append_to_csv_from_dataframe(row, file_path, file_exists):
+    pd.DataFrame([row]).to_csv(
+        file_path,
+        mode='a',
+        sep=",",
+        header=not file_exists, # Write header only if file doesn't exist
+        index=False
+    )
 
 def enrich_url(url, registration_date, session):
     """Process an individual URL to get status code and Open Graph data."""
@@ -77,8 +88,6 @@ def enrich_urls(input_csv_path, output_csv_path):
         if 'url' not in input_data.columns:
             print("CSV must contain a 'url' column")
             return
-
-        new_rows = []
         
         # Create new requests.Session() with desired settings
         session = setup_session()
@@ -89,9 +98,9 @@ def enrich_urls(input_csv_path, output_csv_path):
                 continue # Skip URLs that we've already processed
 
             enriched_row = enrich_url(row['url'], row['registration_date'], session)
-            new_rows.append(enriched_row)
 
             # Convert enriched data to DataFrame and save it
+            print(enriched_row)
             pd.DataFrame([enriched_row]).to_csv(
                 output_csv_path,
                 mode='a',
@@ -107,6 +116,137 @@ def enrich_urls(input_csv_path, output_csv_path):
     except Exception as e:
         print(f"Error during processing: {e}")
 
+async def process_urls(input_data, session, processed_urls, output_csv_path, file_exists):
+
+    tasks_test = [
+        enrich_url_async(row['url'], row['registration_date'], row['nexus_category'], session)
+        for index, row in input_data.iterrows()
+        if row['url'] not in processed_urls
+    ]
+
+    async for task in asyncio.as_completed(tasks_test):
+        result = await task
+        if result:
+            append_to_csv_from_dataframe(result, output_csv_path, file_exists)
+            file_exists = True
+            
+    # tasks = []
+
+    # for index, row in input_data.iterrows():
+    #     if row['url'] in processed_urls:
+    #         continue
+        
+    #     print('process_urls:', row['url'])
+
+    #     tasks.append(enrich_url_async(row['url'], row['registration_date'], row['nexus_category'], session))
+
+    # results = await asyncio.gather(*tasks)
+
+    # print("results:", results)
+
+    # pd.DataFrame(results).to_csv(
+    #     output_csv_path,
+    #     mode='a',
+    #     sep=",",
+    #     header=not file_exists, # Write header only if file doesn't exist
+    #     index=False,
+    # )
+
+async def enrich_url_async(url, registration_date, nexus_category, session):
+
+    # Ensure url starts with https or http protocol
+    url_with_protocol = ensure_protocol(url)
+
+    # print("in enrich_url for", url_with_protocol)
+    try:
+        async with session.get(url_with_protocol) as response:
+            # print('async trying: ', url_with_protocol)
+
+            # Fetch HTTP status code
+            status_code = await get_status_code_async(url_with_protocol, session)
+
+            # status_code = 200
+
+            # Fetch Open Graph metadata
+            # if status_code != "Error":
+            metadata = await get_meta_data_async(url_with_protocol, session)
+
+            # metadata = {
+            #     "final_url": "url.com",
+            #     "title": "title",
+            #     "description": "description",
+            #     "image": "image"
+            # }
+
+            print('url:', url_with_protocol, "status:", status_code)
+
+            return {
+                # Original data
+                'url': url,
+                'registration_date': registration_date,
+                # 'nexus_category': nexus_category,           # Uncomment later!!
+                # Status code from pinging URL
+                'status_code': status_code,
+                # Metadata from URL, or redirected URL
+                'final_url': metadata['final_url'],
+                'title': metadata['title'],
+                'description': metadata['description'],
+                'image': metadata['image']
+            }
+    except Exception as e:
+        print(f"Error processing URL {url}: {e}")
+
+        return {
+            # Original data
+            'url': url,
+            'registration_date': registration_date,
+            # Status code from pinging URL
+            'status_code': "Error",
+            # Metadata from URL, or redirected URL
+            'final_url': "Error",
+            'title': "Error",
+            'description': "Error",
+            'image': "Error"
+        }
+
+async def enrich_urls_async(input_csv_path, output_csv_path):
+
+    # Load existing data if output CSV exists, otherwise create new DataFrame
+    try:
+        file_exists = os.path.isfile(output_csv_path)
+
+        if file_exists:
+            existing_data = pd.read_csv(output_csv_path)
+        else:
+            existing_data = pd.DataFrame(columns=CSV_ROWS_SCHEMA)
+    except FileNotFoundError:
+        existing_data = pd.DataFrame(columns=CSV_ROWS_SCHEMA)
+        
+    try:
+        processed_urls = set(existing_data['url'])
+        input_data = pd.read_csv(input_csv_path)
+
+        if 'url' not in input_data.columns:
+            print("CSV must contain a 'url' column")
+            return
+        
+        async with aiohttp.ClientSession() as session:
+            await process_urls(
+                input_data,
+                session,
+                processed_urls,
+                output_csv_path,
+                file_exists
+            )
+
+            print('finished process_urls')
+        print('finished!')
+
+    except Exception as e:
+        print(f"Error during processing: {e}")
+        
+
+
 # # Test files
 # test_csv_path = 'test_file.csv' # Sample of ~100 rows from the real data
 # test_output_path = 'test_output_file.csv' # Path to save enriched test data
@@ -116,6 +256,18 @@ def enrich_urls(input_csv_path, output_csv_path):
 # real_output_path = 'output_urls_enriched.csv'  # Path to save enriched CSV file
 
 if __name__ == "__main__":
-    input_csv_path = sys.argv[1]
-    output_csv_path = sys.argv[2]
-    enrich_urls(input_csv_path, output_csv_path)
+    parser = argparse.ArgumentParser(
+        prog='EnrichUrls',
+        description='Enrich a set of URLs from an input CSV file with HTTP status code and OpenGraph info available at the URL.'
+    )
+
+    parser.add_argument('input_csv_path', help='Path to input CSV.')
+    parser.add_argument('output_csv_path', help='Path to output CSV.')
+    parser.add_argument('-a', '--asynchronous', action='store_true', help='Run in asynchronous mode. Default False.')
+
+    args = parser.parse_args()
+
+    if args.asynchronous:
+        asyncio.run(enrich_urls_async(args.input_csv_path, args.output_csv_path))
+    else:
+        enrich_urls(args.input_csv_path, args.output_csv_path)
--- utils/populate_metadata.py
+++ utils/populate_metadata.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
+import aiohttp
+import asyncio
 
 def ensure_protocol(url):
     """Ensure the URL starts with 'http://' or 'https://'."""
@@ -71,6 +73,72 @@ def get_meta_data(url, session):
     # If we encountered an exception, return original url value and Error
     return {
             'url': url,
+            'final_url': "Error",
+            'title': "Error",
+            'description': "Error",
+            'image': "Error",
+        }
+
+async def get_status_code_async(url, session):
+    """Fetch the HTTP status code for a given URL."""
+    try:
+        # Try with https
+        async with session.head(url, timeout=5) as response:
+            return response.status
+    except requests.exceptions.SSLError:
+        print(f"SSL error with {url}, trying http instead.")
+        # Try with http if https fails
+        url = url.replace("https://", "http://")
+        try:
+            async with session.head(url, timeout=5) as response:
+                return response.status
+        except requests.RequestException as e:
+            print(f"Error fetching {url}: {e}")
+            return "Error"
+    except requests.RequestException as e:
+        print(f"Error fetching {url}: {e}")
+        return "Error"
+    
+async def get_meta_data_async(url, session):
+    try:
+        async with session.get(url, timeout=5) as response:
+            # response.raise_for_status() # Built-in function to ensure status code is 200
+
+            # Get the final redirected URL
+            final_url = response.url
+
+            soup = BeautifulSoup(await response.text(), 'html.parser')
+
+            # Open Graph metadata
+            og_title = soup.find('meta', property='og:title')
+            og_description = soup.find('meta', property='og:description')
+            og_image = soup.find('meta', property='og:image')
+
+            # Twitter metadata
+            twitter_title = soup.find('meta', attrs={'name': 'twitter:title'})
+            twitter_description = soup.find('meta', attrs={'name': 'twitter:description'})
+            twitter_image = soup.find('meta', attrs={'name': 'twitter:image'})
+
+            # Standard metadata
+            title = soup.title.string if soup.title else 'Not found'
+            description = soup.find('meta', attrs={'name': 'description'})
+
+            # Helper function to safely extract content
+            def get_content(tag):
+                return tag['content'] if tag and tag.has_attr('content') else None
+
+            return {
+                'final_url': final_url,
+                'title': get_content(og_title) or get_content(twitter_title) or title,
+                'description': get_content(og_description) or get_content(twitter_description) or (get_content(description) if description else 'Not found'),
+                'image': get_content(og_image) or get_content(twitter_image) or 'Not found'
+            }
+    except requests.RequestException as e:
+        print(f"Error fetching metadata for {url}: {e}")
+    except Exception as e:
+        print(f"Unexpected error for {url}: {e}")
+    # If we encountered an exception, return original url value and Error
+    return {
             'final_url': "Error",
             'title': "Error",
             'description': "Error",
