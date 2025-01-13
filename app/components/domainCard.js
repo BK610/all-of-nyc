d7862f660392b8c713506a049ade0e10840a7d33
BK610 <bailey.orion.kane@gmail.com>
2025-01-13 17:26:32 -0500

    Use pre-calculated database values for determining if data was found rather than creating local variables

--- app/components/domainCard.js
+++ app/components/domainCard.js
@@ -1,42 +1,17 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 const DomainCard = ({ url }) => {
   if (!url) return null;
 
-  const isUrlFound = !(
-    !url.final_url ||
-    url.final_url == "Error" ||
-    url.final_url == "Not found"
-  );
-
-  // Not using this for now. There are too many false negatives
-  //   for URLs that have bad status codes but still work.
-  // Check if the status code is 2xx or 3xx, indicating a success
-  // let status_code_success =
-  //   url.status_code.substring(0, 1) == 2 ||
-  //   url.status_code.substring(0, 1) == 3;
-
-  const isOGTitleFound = !(
-    !url.title ||
-    url.title == "Error" ||
-    url.title == "Not found"
-  );
-
-  const isOGImageFound = !(
-    !url.image ||
-    url.image == "Error" ||
-    url.image == "Not found"
-  );
-
   return (
     <a
-      href={isUrlFound ? url.final_url : ""}
-      target={isUrlFound ? "_blank" : ""}
+      href={url.is_url_found ? url.final_url : undefined}
+      target={url.is_url_found ? "_blank" : undefined}
       rel="noopener noreferrer"
       className={`inline-block w-full mb-4 overflow-hidden bg-white shadow-lg rounded-lg border border-gray-200 transition-transform transform hover:scale-105 hover:shadow-xl
-        ${isUrlFound ? "" : "pointer-events-none bg-gray-100 opacity-70"}`} // Set disabled-esque styling if found_url is false
+        ${!url.is_url_found && "pointer-events-none bg-gray-100 opacity-70"}`} // Set disabled-esque styling if found_url is false
     >
-      {isOGImageFound ? (
+      {url.is_og_image_found ? (
         <div className="w-full">
           <img
             className="object-cover w-full h-52 border-b-2"
@@ -51,13 +26,13 @@ const DomainCard = ({ url }) => {
       <div className="p-5">
         <h2 className="text-nyc-blue text-xl font-semibold mb-2">
           {url.domain_name}&nbsp;
-          {isUrlFound ? <>{isOGTitleFound ? "✅" : "❓"}</> : "💀"}
+          {url.is_url_found ? <>{url.is_og_title_found ? "✅" : "❓"}</> : "💀"}
         </h2>
         <p className="mb-1">
-          <b>Title:</b> {isUrlFound ? url.title : "N/A"}
+          <b>Title:</b> {url.is_url_found ? url.title : "N/A"}
         </p>
         <p className="mb-1 line-clamp-3">
-          <b>Description:</b> {isUrlFound ? url.description : "N/A"}
+          <b>Description:</b> {url.is_url_found ? url.description : "N/A"}
         </p>
         <p className="text-gray-600 pt-2 w-full border-t-2">
           Final URL: {url.final_url}
