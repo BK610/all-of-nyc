02cbb4e64794eecec69addc3ffb26ceef410711e
BK610 <bailey.orion.kane@gmail.com>
2025-01-13 18:01:43 -0500

    Create basic filter to filter just on website status

---
+++ app/components/filter.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import Button from "./button";
+
+const Filter = ({ onFilter }) => {
+  const [filters, setFilters] = useState({
+    status: "is_complete",
+  });
+
+  const handleFilter = (e) => {
+    e.preventDefault();
+    // console.log(filters);
+    onFilter(filters);
+  };
+
+  return (
+    <form
+      onSubmit={handleFilter}
+      className="mb-2 flex justify-center items-center gap-4"
+    >
+      {/* <div>Filters</div> */}
+      <div className="p-2 flex flex-col justify-center border border-gray-300 rounded-lg">
+        <label className="text-gray-500" for="website-status-select">
+          Website status
+        </label>
+        <select
+          className="px-2 py-2 border-b-2 border-gray-300 text-sm"
+          name="status"
+          id="status-select"
+          onChange={(e) => setFilters({ ...filters, status: e.target.value })}
+        >
+          <option value="is_complete">✅ Complete</option>
+          <option value="is_live">❓ Live</option>
+          <option value="is_down">💀 Down</option>
+        </select>
+      </div>
+      <Button type="submit">Filter</Button>
+    </form>
+  );
+};
+
+export default Filter;
