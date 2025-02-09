bd25b530cdc79870995e4105d2537e4771b72afa
BK610 <bailey.orion.kane@gmail.com>
2025-02-09 14:27:57 -0500

    Set up loggin

--- scripts/utils/get_open_api_data.py
+++ scripts/utils/get_open_api_data.py
@@ -11,6 +11,9 @@ class OpenApiDataProcessor:
             open_api_url:   The Open API URL from which to retrieve data
             batch_size:     The number of records to retrieve in each API call. Defaults to 1000
         """
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
+
         self.open_api_url = open_api_url
         self.batch_size = batch_size
         
@@ -19,11 +22,8 @@ class OpenApiDataProcessor:
         elif open_api_url[len(open_api_url)-4:] == 'json':
             self.mode = 'json'
         else:
-            print(f"Unrecognized URL format: {open_api_url}. Failed to create OpenApiDataProcessor.")
+            self.logger.error(f"Unrecognized URL format: {open_api_url}. Failed to create OpenApiDataProcessor.")
             return None
-        
-        logging.basicConfig(level=logging.INFO)
-        self.logger = logging.getLogger(__name__)
 
     def get_data(self) -> pd.DataFrame:
         """Collect all available data from the provided NYC Open API URL.
@@ -37,7 +37,7 @@ class OpenApiDataProcessor:
             url_to_call = self.open_api_url + "?$order=:id&$offset=" + str(offset)
             url_to_call = url_to_call.replace(" ", "%20")
             
-            print(f"Retrieving data from {url_to_call}")
+            self.logger.info(f"Retrieving data from {url_to_call}")
 
             match self.mode:
                 case 'csv':
@@ -46,15 +46,15 @@ class OpenApiDataProcessor:
                     next_data = pd.read_json(url_to_call)
 
             if not next_data.empty:
-                print(f"Collecting from row {offset}")
+                self.logger.debug(f"Collecting from row {offset}")
                 df = pd.concat([df, next_data])
                 offset += self.batch_size
-                print(f"Collected {self.batch_size} rows.")
+                self.logger.debug(f"Collected {self.batch_size} rows.")
             else:
                 more_data = False
-                print("No more data available.")
+                self.logger.debug("No more data available.")
 
-        print(f"Collected all available data from {self.open_api_url}")
+        self.logger.info(f"Collected all available data from {self.open_api_url}")
         return df
 
     def get_data_by_date(self, start_date, end_date = datetime.utcnow().isoformat()) -> pd.DataFrame:
@@ -64,12 +64,12 @@ class OpenApiDataProcessor:
         df = pd.DataFrame()
         offset = 0
         more_data = True
+        
+        self.logger.info(f"Retrieving data from {self.open_api_url} from {start_date} to {end_date}")
 
         while(more_data):
             url_to_call = self.open_api_url + "?$order=:id&$offset=" + str(offset) + "&$where=domain_registration_date BETWEEN '" + str(start_date) + "' AND '" + str(end_date) + "'"
             url_to_call = url_to_call.replace(" ", "%20")
-
-            print(f"Retrieving data from {url_to_call}")
 
             match self.mode:
                 case 'csv':
@@ -78,13 +78,13 @@ class OpenApiDataProcessor:
                     next_data = pd.read_json(url_to_call)
 
             if not next_data.empty:
-                print(f"Collecting from row {offset}")
+                self.logger.debug(f"Collecting from row {offset}")
                 df = pd.concat([df, next_data])
                 offset += self.batch_size
-                print(f"Collected {self.batch_size} rows.")
+                self.logger.debug(f"Collected {self.batch_size} rows.")
             else:
                 more_data = False
-                print("No more data available.")
+                self.logger.debug("No more data available.")
 
-        print(f"Collected all available data from {self.open_api_url}")
+        self.logger.info(f"Collected all available data from {self.open_api_url}")
         return df
