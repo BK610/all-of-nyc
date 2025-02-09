e5706d57c3472a40ba9e8a100be4bdcbd0beda3d
BK610 <bailey.orion.kane@gmail.com>
2025-02-09 09:53:01 -0500

    Remove Geist fonts

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,5 @@
-import localFont from "next/font/local";
 import "./globals.css";
 import { CSPostHogProvider } from "./providers";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "All of .nyc",
@@ -24,11 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <CSPostHogProvider>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
+        <body className={`antialiased`}>{children}</body>
       </CSPostHogProvider>
     </html>
   );
