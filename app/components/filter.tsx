be6d459dd663ab82922206aa171eae378ce63e67
Bailey Kane <bailey.orion.kane@gmail.com>
2025-04-11 14:26:18 -0400

    Update button import

--- app/components/filter.tsx
+++ app/components/filter.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Button from "./button";
+import Button from "@/components/button";
 
 export default function Filter({ onFilter }) {
   const [filters, setFilters] = useState({
