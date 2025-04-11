bdd95dabfd90ee8505fd7932dd8df4c4c05a58be
Bailey Kane <bailey.orion.kane@gmail.com>
2025-04-11 14:01:02 -0400

    Migrate search.js to tsx

---
+++ app/components/search.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import Button from "./button";
+
+interface SearchProps {
+  onSearch: (query: string) => void;
+  className?: string;
+}
+
+const Search = ({ onSearch, className }: SearchProps): React.ReactElement => {
+  const [query, setQuery] = useState("");
+
+  // Handle search submission
+  const handleSearch = (e) => {
+    e.preventDefault();
+    onSearch(query);
+  };
+
+  return (
+    <form
+      onSubmit={handleSearch}
+      className={`${className} mb-2 flex justify-center items-center`}
+    >
+      <input
+        id="search"
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search all of .nyc"
+        className="w-full px-4 py-2 border border-gray-300 rounded-l-lg text-gray-900 placeholder-gray-500"
+      />
+      <Button className={"rounded-r-lg"} type="submit">
+        Search
+      </Button>
+    </form>
+  );
+};
+
+export default Search;
