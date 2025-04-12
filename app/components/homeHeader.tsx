27f66dc20ac4c727def9ea1692851a62a0146e34
Bailey Kane <bailey.orion.kane@gmail.com>
2025-04-11 20:41:31 -0400

    Remove 'built by' text, and move to footer in layout

--- app/components/homeHeader.tsx
+++ app/components/homeHeader.tsx
@@ -3,7 +3,7 @@ import nycLogo from "/public/nycLogo.png";
 
 export default function HomeHeader(): React.ReactElement {
   return (
-    <div className="border-b-2 mb-4 text-center">
+    <div className="mb-4 text-center">
       <h1 className="text-3xl font-bold text-nyc-blue mb-2">
         All of{" "}
         <Image
@@ -13,17 +13,6 @@ export default function HomeHeader(): React.ReactElement {
         />
       </h1>
       <h2 className="text-xl mb-2">Discover how .nyc domains are used.</h2>
-      <p className="text-base text-gray-600 mb-1">
-        Built by{" "}
-        <a
-          className="underline"
-          href="https://www.baileykane.co/?ref=allof.nyc"
-          target="_blank"
-        >
-          Bailey Kane
-        </a>
-        .
-      </p>
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer";
 import "./globals.css";
 import { CSPostHogProvider } from "./providers";
 
@@ -13,7 +14,8 @@ export default function RootLayout({ children }): React.ReactElement {
     <html lang="en">
       <CSPostHogProvider>
         <body className={`bg-amber-50 antialiased`}>
-          <div className="max-w-6xl mx-auto py-10 px-4">{children}</div>
+          <div className="max-w-6xl mx-auto pt-10 pb-2 px-4">{children}</div>
+          <Footer />
         </body>
       </CSPostHogProvider>
     </html>
