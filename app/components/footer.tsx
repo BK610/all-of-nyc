3d6159f38146acef9c142f1bf20ac6150cfe44ec
Bailey Kane <bailey.orion.kane@gmail.com>
2025-04-11 22:12:55 -0400

    Add index to footer elements

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -23,8 +23,8 @@ export default function Footer({}: FooterProps): React.ReactElement {
   return (
     <footer className="w-full py-6 h-fit text-center flex flex-col">
       <div className="mx-auto flex flex-col sm:flex-row gap-4 sm:gap-2 text-sm">
-        {links.map((link) => (
-          <div className="m-auto">
+        {links.map((link, index) => (
+          <div className="m-auto" key={index}>
             <a
               className="px-2 py-1 rounded-sm shadow-lg hover:shadow-2xl
           hover:outline-2 outline-primary bg-gradient-to-br from-amber-200 to-amber-300 hover:from-amber-100 hover:to-amber-200
