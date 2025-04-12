ec5c6a6acff4a99fd0c166bfc76bb06aa4021854
Bailey Kane <bailey.orion.kane@gmail.com>
2025-04-11 22:08:10 -0400

    Improve loading screen styling

--- app/components/queryResultsList.tsx
+++ app/components/queryResultsList.tsx
@@ -10,10 +10,15 @@ export default function QueryResultsList({
   loading,
 }: QueryResultsListProps): React.ReactElement {
   return (
-    <div className="columns-1 md:columns-2 lg:columns-3 space-y-4">
-      {loading
-        ? [...Array(15)].map(() => <SkeletonDomainCard />)
-        : urls.map((url, index) => <DomainCard key={index} url={url} />)}
+    <div className="flex flex-col gap-2">
+      <p className={`w-full text-center ${loading ? "visible" : "invisible"}`}>
+        <b>Loading some sweet, sweet data...</b>
+      </p>
+      <div className="columns-1 md:columns-2 lg:columns-3 space-y-4">
+        {loading
+          ? [...Array(15)].map((index) => <SkeletonDomainCard key={index} />)
+          : urls.map((url, index) => <DomainCard key={index} url={url} />)}
+      </div>
     </div>
   );
 }
