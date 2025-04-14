c875b57547133afba8bac77ddd62a5d0751df384
Bailey Kane <bailey.orion.kane@gmail.com>
2025-04-13 22:44:57 -0400

    Move footer to the bottom of the page using flexbox

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,10 @@ export default function RootLayout({ children }): React.ReactElement {
   return (
     <html lang="en">
       <CSPostHogProvider>
-        <body className={`bg-amber-50 antialiased`}>
-          <main className="max-w-6xl mx-auto pt-10 pb-2 px-4">{children}</main>
+        <body className={`bg-amber-50 antialiased min-h-screen flex flex-col`}>
+          <main className="flex-1 w-full max-w-6xl mx-auto pt-10 pb-2 px-4">
+            {children}
+          </main>
           <Footer />
           <Toaster position="top-right" />
         </body>
