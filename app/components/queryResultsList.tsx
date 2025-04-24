c7b44a07525367e51193bb6957e0b15e0447a1b2
Bailey Kane <bailey.orion.kane@gmail.com>
2025-04-24 14:11:17 -0400

    Add text with total URL count

--- app/components/queryResultsList.tsx
+++ app/components/queryResultsList.tsx
@@ -3,17 +3,30 @@ import DomainCard, { SkeletonDomainCard } from "@/components/domainCard";
 interface QueryResultsListProps {
   urls: any[];
   loading: boolean;
+  totalUrlsCount: number;
 }
 
 export default function QueryResultsList({
   urls,
   loading,
+  totalUrlsCount,
 }: QueryResultsListProps): React.ReactElement {
   return (
-    <div className="columns-1 md:columns-2 lg:columns-3 space-y-4">
-      {loading
-        ? [...Array(15)].map((index) => <SkeletonDomainCard key={index} />)
-        : urls.map((url) => <DomainCard key={url.domain_name} url={url} />)}
+    <div>
+      <p className={`w-full text-center pb-2`}>
+        {loading ? (
+          <>Loading some sweet, sweet data...</>
+        ) : (
+          <>
+            Found <b>{totalUrlsCount}</b> matching <code>.nyc</code> domains.
+          </>
+        )}
+      </p>
+      <div className="columns-1 md:columns-2 lg:columns-3 space-y-4">
+        {loading
+          ? [...Array(15)].map((index) => <SkeletonDomainCard key={index} />)
+          : urls.map((url) => <DomainCard key={url.domain_name} url={url} />)}
+      </div>
     </div>
   );
 }
