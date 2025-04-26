d999adb9d8af9f0c101c3a6886fce6f703383bac
Bailey Kane <bailey.orion.kane@gmail.com>
2025-04-26 13:21:26 -0400

    Make logo a link to the homepage. Add a link to the about page

--- app/components/homeHeader.tsx
+++ app/components/homeHeader.tsx
@@ -1,28 +1,35 @@
 import Image from "next/image";
 import nycLogo from "/public/nycLogo.png";
-
+import Link from "next/link";
 export default function HomeHeader(): React.ReactElement {
   return (
-    <div className="mb-4 text-center">
-      <h1 className="text-3xl font-bold text-nyc-blue mb-2">
-        All of{" "}
-        <Image
-          className="inline w-20 h-20"
-          src={nycLogo}
-          alt="Logo of the .nyc domain registration program."
-        />
-      </h1>
-      <h2 className="text-xl mb-2">
-        Discover how{" "}
-        <a
-          href="https://www.ownit.nyc/"
-          target="_blank"
-          className="bg-gray-50 rounded-md px-1 outline outline-nyc-medium-gray hover:outline-nyc-blue"
-        >
-          <code className="">.nyc</code>
-        </a>{" "}
-        is used.
-      </h2>
-    </div>
+    <section>
+      <div className="max-w-6xl">
+        <Link href="/about">What's this about?</Link>
+      </div>
+      <div className="mb-4 text-center w-fit mx-auto">
+        <Link href="/" className="w-fit mx-auto hover:underline">
+          <h1 className="text-3xl font-bold text-nyc-blue mb-2">
+            All of{" "}
+            <Image
+              className="inline w-20 h-20"
+              src={nycLogo}
+              alt="Logo of the .nyc domain registration program."
+            />
+          </h1>
+        </Link>
+        <h2 className="text-xl mb-2">
+          Discover how{" "}
+          <a
+            href="https://www.ownit.nyc/"
+            target="_blank"
+            className="bg-gray-50 rounded-md px-1 outline outline-nyc-medium-gray hover:outline-nyc-blue"
+          >
+            <code className="">.nyc</code>
+          </a>{" "}
+          is used.
+        </h2>
+      </div>
+    </section>
   );
 }
