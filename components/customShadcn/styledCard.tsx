d82a31bee18743e0c67b3508a3d2446b096d09e9
Bailey Kane <bailey.orion.kane@gmail.com>
2025-05-05 20:12:08 -0400

    Create shadcn Card wrapper with styling

---
+++ components/customShadcn/styledCard.tsx
@@ -0,0 +1,36 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+  CardDescription,
+  CardAction,
+} from "@/components/ui/card";
+
+interface CustomCardProps {
+  className?: string;
+  children: React.ReactNode;
+  [key: string]: any;
+}
+
+export default function CustomCard({
+  className,
+  children,
+  ...props
+}: CustomCardProps): React.ReactElement {
+  return (
+    <Card
+      className={`${className} gap-2 w-full cursor-pointer overflow-hidden text-primary rounded-lg shadow-lg hover:shadow-xl transition-all duration-75
+        outline outline-nyc-medium-gray hover:outline-nyc-blue focus:outline-nyc-orange focus:outline-4 focus-within:outline-4 focus-within:outline-nyc-orange hover:focus-within:outline-nyc-orange
+        bg-gradient-to-br from-gray-50 to-gray-50
+        hover:not-focus:to-amber-50
+        focus:from-white focus:to-amber-100
+        focus-within:from-white focus-within:to-amber-100
+        hover:focus-within:from-white hover:focus-within:to-amber-100`}
+      {...props}
+    >
+      {children}
+    </Card>
+  );
+}
