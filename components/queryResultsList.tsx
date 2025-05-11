13b8a68fd217468478bdc49c4a5e7fcd392b83d6
Bailey Kane <bailey.orion.kane@gmail.com>
2025-05-11 10:51:52 -0400

    Remove layoutId prop passed to DomainCard

--- components/queryResultsList.tsx
+++ components/queryResultsList.tsx
@@ -26,13 +26,7 @@ export default function QueryResultsList({
       <div className="columns-1 md:columns-2 lg:columns-3 space-y-4">
         {loading
           ? [...Array(15)].map((index) => <SkeletonDomainCard key={index} />)
-          : urls.map((url) => (
-              <DomainCard
-                key={url.domain_name}
-                url={url}
-                layoutId={url.domain_name}
-              />
-            ))}
+          : urls.map((url) => <DomainCard key={url.domain_name} url={url} />)}
       </div>
     </div>
   );
