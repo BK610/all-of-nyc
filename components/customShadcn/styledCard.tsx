3e45c1a6aa46228ada1e2f3975056c7a8c8defbe
Bailey Kane <bailey.orion.kane@gmail.com>
2025-05-11 11:12:02 -0400

    Remove cursor-pointer styling, make optional

--- components/customShadcn/styledCard.tsx
+++ components/customShadcn/styledCard.tsx
@@ -21,7 +21,7 @@ export default function StyledCard({
 }: CustomCardProps): React.ReactElement {
   return (
     <Card
-      className={`${className} gap-2 w-full cursor-pointer overflow-hidden text-primary rounded-lg shadow-lg hover:shadow-xl transition-all duration-75
+      className={`${className} gap-2 w-full overflow-hidden text-primary rounded-lg shadow-lg hover:shadow-xl transition-all duration-75
         outline outline-nyc-medium-gray hover:outline-nyc-blue focus:outline-nyc-orange focus:outline-4 focus-within:outline-4 focus-within:outline-nyc-orange hover:focus-within:outline-nyc-orange
         bg-gradient-to-br from-gray-50 to-gray-50
         hover:not-focus:to-amber-50
