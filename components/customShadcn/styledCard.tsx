1c906f2af3f537a85cdd0cd9b1f01433ac90dee4
Bailey Kane <bailey.orion.kane@gmail.com>
2025-05-11 13:35:18 -0400

    Remove unused imports. Use cn for classname conflict resolution

--- components/customShadcn/styledCard.tsx
+++ components/customShadcn/styledCard.tsx
@@ -1,12 +1,5 @@
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-  CardFooter,
-  CardDescription,
-  CardAction,
-} from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/utils/shadcn.utils";
 
 interface CustomCardProps {
   className?: string;
@@ -21,13 +14,16 @@ export default function StyledCard({
 }: CustomCardProps): React.ReactElement {
   return (
     <Card
-      className={`${className} gap-2 w-full overflow-hidden text-primary rounded-lg shadow-lg hover:shadow-xl transition-all duration-75
+      className={cn(
+        `gap-2 w-full overflow-hidden text-primary rounded-lg shadow-lg hover:shadow-xl transition-all duration-75
         outline outline-nyc-medium-gray hover:outline-nyc-blue focus:outline-nyc-orange focus:outline-4 focus-within:outline-4 focus-within:outline-nyc-orange hover:focus-within:outline-nyc-orange
         bg-gradient-to-br from-gray-50 to-gray-50
         hover:not-focus:to-amber-50
         focus:from-white focus:to-amber-100
         focus-within:from-white focus-within:to-amber-100
-        hover:focus-within:from-white hover:focus-within:to-amber-100`}
+        hover:focus-within:from-white hover:focus-within:to-amber-100`,
+        className
+      )}
       {...props}
     >
       {children}
